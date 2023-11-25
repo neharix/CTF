bab87f34d4a7958abc9c51f928b62c6b2207101e
neharix <azatjurjenov24@gmail.com>
2023-11-25 22:38:01 +0500

    admin tools are upgraded

--- admin_tools/urls.py
+++ admin_tools/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.admin_tools, name="admin_tools"),
-    path("register_tool/", views.register_tools, name="admin_register_tools"),
+    path("register_tool/", views.register_tools, name="register_tools"),
 ]
